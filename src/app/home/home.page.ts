fe91df4c221a9c323c6f6b6d7e1130839c21d1e4
steven <sriver@hotmail.fr>
2024-06-07 14:38:04 +0200

    test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,18 +6,25 @@ import { NavController } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
   selectedImage: string = '../../assets/images/halo.png';
- 
+  backgroundColor: string = 'white';
+
   constructor(private navCtrl: NavController) {}
- 
+
   navigate(path: string) {
     this.navCtrl.navigateForward(`${path}`);
   }
+
+  changeBackgroundColor(color: string) {
+    this.backgroundColor = color;
+  }
+
   changFond(image: string) {
     console.log(image);
     this.selectedImage = image;
- 
+    this.changeBackgroundColor('blue');
   }
 }
 
